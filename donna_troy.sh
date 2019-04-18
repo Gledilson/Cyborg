c61a01947f451590bcdc31c33fe20af540a503f3
Gledilson <gledilsonavelar@hotmail.com>
2019-04-18 01:22:39 +0000

    add

--- donna_troy.sh
+++ donna_troy.sh
@@ -2,5 +2,5 @@
 for IP in 10 20 30 40
 do 
 
-	ping -c2 192.168.99.$IP &> /dev/null && echo "SHAZAM!" || echo "BLAM!" 
+	ping -c2 192.168.99.$IP &> /dev/null && echo "$IP SHAZAM!" || echo "$IP BLAM!" 
 done
